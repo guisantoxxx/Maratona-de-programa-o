f159052757a59588ab9cdf9e11d141811f1d7218
Guilherme <guilherme.santoss1802@gmail.com>
2024-10-22 18:55:25 -0300

    correcao do nome

---
+++ MINI-MARATONA 1/twins.c
@@ -0,0 +1,37 @@
+//https://codeforces.com/problemset/problem/160/A
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+ 
+int main() {
+ 
+  int numberOfCoins;
+  int coinsValues[100];
+  float totalSum = 0;
+  int cont = 0;
+ 
+  scanf("%d", &numberOfCoins);
+ 
+  for(int i = 0; i < numberOfCoins; i++) {
+    scanf("%d", &coinsValues[i]);
+    totalSum += coinsValues[i];
+  }
+ 
+  float half = totalSum/2;
+  int resultSum = 0;
+ 
+  qsort(coinsValues, numberOfCoins, sizeof(int), compare);
+  
+  for(int i = numberOfCoins - 1; resultSum <= half; i--) {
+    resultSum += coinsValues[i];
+    cont++;
+  }
+ 
+  printf("%d", cont);
+ 
+  return 0;
+}
